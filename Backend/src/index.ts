dee4e0a196bafc585e5696fe98e0cd6d689b32f4
Brian Chiu <bkchiu0@gmail.com>
2019-12-15 15:38:55 -0500

    Add boilerplate code and updates

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -8,22 +8,28 @@ const app = express();
 const port = 27017;
 const host = "192.168.99.100";
 
-// Replace values with more appropriate values.
-mongoose.connect(`mongodb://${host}:${port}/db`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+async function connect() {
+  const connection = await mongoose.connect(`mongodb://${host}:${port}/db`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 
-// Enable CORS
-app.use(cors());
-// Parses incoming requests as JSON if parsable
-app.use(bodyParser.json());
-// Compresses all response json bodies
-app.use(compression());
+  // Enable CORS
+  app.use(cors());
+  // Parses incoming requests as JSON if parsable
+  app.use(bodyParser.json());
+  // Compresses all response json bodies
+  app.use(compression());
 
-// Add middleware here
-app.use((req, res, next) => {
-  next();
-});
+  return Promise.resolve();
+}
 
-app.listen(port, () => console.log(`Node is now listening on ${host}:${port}`));
+connect()
+  .then(() => {
+    app.listen(port, () =>
+      console.log(`Node is now listening on ${host}:${port}`)
+    );
+  })
+  .catch(() => {
+    console.error("Node failed to initialize.");
+  });
