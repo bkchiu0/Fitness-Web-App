04fa2aa86a6883c680e5796e79cd5ad5668bf87c
Brian Chiu <bkchiu0@gmail.com>
2019-12-15 21:46:43 -0500

    Implement auth handler

--- Backend/src/handlers/authentication.ts
+++ Backend/src/handlers/authentication.ts
@@ -1,4 +1,6 @@
 import jwt from "jsonwebtoken";
+import uuid from "uuid/v4";
+import bcrypt from "bcryptjs";
 
 import UserModel from "../models/user";
 import IUser from "../Interfaces/IUser";
@@ -13,22 +15,51 @@ export interface IAuthHandler {
    * @param user the user to be created
    */
   createUser(user: IUser): Promise<{ token: string; uuid: string }>;
-
-  /**
-   * Generates an authentication token using user as a payload
-   * @param user the payload for jwt generation
-   */
-  generateAuthToken(user: IUser): string;
 }
 
 class AuthHandler implements IAuthHandler {
   constructor() {}
 
   async createUser(user: IUser): Promise<{ token: string; uuid: string }> {
-    return Promise.resolve({ token: "", uuid: "" });
+    const id = uuid();
+    user.uuid = id;
+
+    // Create and validate user model
+    let model = new UserModel(user);
+    await model.validate(err => {
+      const { firstName, lastName, email, password } = err.errors;
+      if (firstName.message) {
+        throw new Error(firstName.message);
+      }
+      if (lastName.message) {
+        throw new Error(lastName.message);
+      }
+      if (email.message) {
+        throw new Error(email.message);
+      }
+      if (password.message) {
+        throw new Error(password.message);
+      }
+      throw new Error("Unknown validation error.");
+    });
+
+    // Check for duplicates
+    let dup = await UserModel.findOne({ email: user.email });
+    if (dup) {
+      throw new Error("Email already exists");
+    }
+
+    model.password = await bcrypt.hash(model.password, 10);
+    await model.save(err => {
+      throw new Error("Failed to save user");
+    });
+
+    // Create a jwt token
+    const token = this.generateAuthToken(user);
+    return Promise.resolve({ token, uuid: id });
   }
 
-  generateAuthToken(user: IUser): string {
+  private generateAuthToken(user: IUser): string {
     return jwt.sign(user, config.privateKey, { expiresIn: "1h" });
   }
 }
--- Backend/src/models/user.ts
+++ Backend/src/models/user.ts
@@ -4,26 +4,34 @@ import IUser from "../Interfaces/IUser";
 const User = new mongoose.Schema({
   firstName: {
     type: String,
+    minlength: 2,
+    maxlength: 50,
     required: [true, "Error: no first name provided."]
   },
   lastName: {
     type: String,
+    minlength: 2,
+    maxlength: 50,
     required: [true, "Error: no last name provided."]
   },
   email: {
     type: String,
-    required: [true, "Error: no email provided."],
-    index: true
+    minlength: 3,
+    maxlength: 255,
+    required: [true, "Error: no email provided."]
   },
   password: {
     type: String,
+    minlength: 8,
+    maxlength: 255,
     required: [true, "Error: no password provided."]
   },
   uuid: {
     type: String,
-    required: [true, "Error: no email provided."],
-    index: true
+    required: [true, "Error: no UUID provided."]
   }
 });
+
+User.index({ email: 1 }, { unique: true });
 
 export default mongoose.model<IUser & mongoose.Document>("User", User);
