1f54da088779394554a6b9916808f242c5a921e3
Brian Chiu <bkchiu0@gmail.com>
2020-01-11 13:07:49 -0500

    Init React frontend

---
+++ Frontend/src/pages/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Login from "./auth/Login";
+
+import "./App.css";
+
+const App: React.FC = () => {
+  return <div className="App">Placeholder for now</div>;
+};
+
+export default App;
